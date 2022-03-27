6b232dfaf2a76757213f97c62feba505b5f89612
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2022-03-27 00:46:13 -0400

    Add chatbox in frontend locally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,17 @@ import axios from "axios";
 
 const App = () => {
   const [location,setLocation] = useState({lat:0,lng:0})
+  const [userChat,setUserChat] = useState("")
+  const [botResponse,setBotResponse] = useState("")
+
+  const getChatBotResponse = () => {
+    let botUrl = "http://127.0.0.1:8000"
+    axios
+    .post(`${botUrl}/api/chatwithbot`, {message: userChat})
+    .then((res) => {
+      setBotResponse(res.data.message)
+    })
+  }
 
   useEffect(()=> {
     navigator.geolocation.getCurrentPosition((position)=>{
@@ -14,6 +25,13 @@ const App = () => {
   return (
     <main className="container">
       <PlacesWithStandaloneSearchBox lat={location.lat} lng={location.lng} />
+      <div style={{border: "solid 2px black", margin:"15px", textAlign: "center"}}>
+        <input style={{width:"700px",margin:"5px"}} type="text" value={userChat} onChange={e => setUserChat(e.target.value)} /> 
+        <button onClick={getChatBotResponse}> Send </button>
+        <p></p>
+        <span style={{fontWeight:"bold"}}>SUPPORT BOT RESPONSE: </span>
+        <span>{botResponse}</span>
+      </div>
     </main>
   );
 }
